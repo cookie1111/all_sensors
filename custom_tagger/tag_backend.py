a087ecf8aca88dfe199b7eba58b97ad9f7490ace
Sebastjan Kramar <sebastjan.kramar@gmail.com>
2024-01-12 13:45:28 +0100

    finsihed a simple tagging interface

--- custom_tagger/tag_backend.py
+++ custom_tagger/tag_backend.py
@@ -4,91 +4,26 @@ import pylsl
 
 
 # SELECT DATA TO STREAM
-tags = []
+LSL_streams = {}
 
 serverAddress = '127.0.0.1'
 serverPort = 28000
 bufferSize = 4096
 
 
-def prepare_LSL_streaming():
+def prepare_LSL_streaming(tags):
     print("Starting LSL streaming")
-    if tags:
-        infoTag = pylsl.StreamInfo('tag','Tag',1,channel_format='float32',source_id='Task_tag');
-        global outletTag
+    for tag in tags:
+        print(f"creating stream {tag}")
+        infoTag = pylsl.StreamInfo(tag,'Tag',1,channel_format='int32',source_id='Task_tag');
         outletTag = pylsl.StreamOutlet(infoTag)
-prepare_LSL_streaming()
+        LSL_streams[tag] = (infoTag,outletTag)
+    return LSL_streams
 
-time.sleep(1)
 
+def stream(button):
 
-def reconnect():
-    print("Reconnecting...")
-    connect()
-    suscribe_to_data()
-    stream()
-
-def stream():
-    last_ibi_time = None  # Variable to store the time of the last IBI event
-    frequency = None
-    try:
-        print("Streaming...")
-        while True:
-            try:
-                response = s.recv(bufferSize).decode("utf-8")
-                #print(response)
-                if "connection lost to device" in response:
-                    #print(response.decode("utf-8"))
-                    reconnect()
-                    break
-                samples = response.split("\n")
-                #print(samples)
-                for i in range(len(samples)-1):
-                    stream_type = samples[i].split()[0]
-                    
-                    if stream_type == "E4_Acc":
-                        timestamp = float(samples[i].split()[1].replace(',','.'))
-                        data = [int(samples[i].split()[2].replace(',','.')), int(samples[i].split()[3].replace(',','.')), int(samples[i].split()[4].replace(',','.'))]
-                        outletACC.push_sample(data, timestamp=timestamp)
-                    if stream_type == "E4_Bvp":
-                        timestamp = float(samples[i].split()[1].replace(',','.'))
-                        data = float(samples[i].split()[2].replace(',','.'))
-                        #print(timestamp)
-                        outletBVP.push_sample([data], timestamp=timestamp)
-                        #print("E4_bvp")
-                    if stream_type == "E4_Ibi":
-                        timestamp = float(samples[i].split()[1].replace(',','.'))
-                        data = float(samples[i].split()[2].replace(',','.'))
-                        outletIBI.push_sample([data], timestamp=timestamp)
-                    if stream_type == "E4_Gsr":
-                        timestamp = float(samples[i].split()[1].replace(',','.'))
-                        data = float(samples[i].split()[2].replace(',','.'))
-                        outletGSR.push_sample([data], timestamp=timestamp)
-                    if stream_type == "E4_Temperature":
-                        timestamp = float(samples[i].split()[1].replace(',','.'))
-                        data = float(samples[i].split()[2].replace(',','.'))
-                        outletTemp.push_sample([data], timestamp=timestamp)
-                    if stream_type == "E4_IbiXDXD": #delete XDXD if you want to check the ibi frequency
-                        timestamp = float(samples[i].split()[1].replace(',','.'))
-                        data = float(samples[i].split()[2].replace(',','.'))                        
-                        if last_ibi_time is not None:
-                            interval = timestamp - last_ibi_time  # Calculate time interval between IBIs
-                            frequency = 1 / interval  # Calculate frequency in Hertz (Hz)
-                            print(f"IBI Frequency: {frequency} Hz length of data: {len(data) if type(data) == list else 1 }")
-                        last_ibi_time = timestamp  # Update the last IBI time
-                    if stream_type == "E4_Tag":
-                        timestamp = float(samples[i].split()[1].replace(',','.'))
-                        data = float(samples[i].split()[2].replace(',','.'))
-                        print(samples[i])
-                        print(data,timestamp)
-                        outletTag.push_sample([data], timestamp=timestamp)
-                #time.sleep(1)
-            except socket.timeout:
-                print("Socket timeout")
-                reconnect()
-                break
-    except KeyboardInterrupt:
-        print("Disconnecting from device")
-        s.send("device_disconnect\r\n".encode())
-        s.close()
-stream()
+    print(f"Streaming {button} label")
+    
+    timestamp = time.time()
+    LSL_streams[button][1].push_sample([1],timestamp=timestamp) 
--- custom_tagger/tag_interface.py
+++ custom_tagger/tag_interface.py
@@ -1,11 +1,19 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLineEdit
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLineEdit, QGridLayout, QHBoxLayout
+from PyQt5.QtCore import Qt
+import tag_backend 
 
 class ButtonCreator(QWidget):
     def __init__(self):
         super().__init__()
         self.created_buttons = {}
+        self.created_streams = {}
+        self.button_grid_layout = QGridLayout()
         self.initUI()
+        
+        self.current_row = 0
+        self.current_column = 0
+        self.max_columns = 8
         
 
     def initUI(self):
@@ -13,33 +21,73 @@ class ButtonCreator(QWidget):
         self.setGeometry(300, 300, 250, 180)
 
         # Layout
-        self.vbox = QVBoxLayout()
+        main_vbox = QVBoxLayout()
 
-        # Text field
+        top_hbox = QHBoxLayout()
         self.text_field = QLineEdit(self)
         self.text_field.setPlaceholderText("Type button name")
-        self.vbox.addWidget(self.text_field)
+        top_hbox.addWidget(self.text_field)
 
         # Create button
         self.create_button = QPushButton('Create', self)
         self.create_button.clicked.connect(self.createButton)
-        self.vbox.addWidget(self.create_button)
+        top_hbox.addWidget(self.create_button)
 
-        self.setLayout(self.vbox)
+        # setup stream button
+        self.stream_button = QPushButton('Start', self)
+        self.stream_button.clicked.connect(self.createStreams)
+        top_hbox.addWidget(self.stream_button)
+
+        main_vbox.addLayout(top_hbox)
+
+        self.button_grid_layout.setSpacing(10)
+        main_vbox.addLayout(self.button_grid_layout)
+
+        self.setLayout(main_vbox)
 
     def createButton(self):
         button_name = self.text_field.text()
         
-        if button_name:
+        if button_name and button_name not in self.created_buttons.keys():
             # Limit the number of buttons to 64
-            if len(self.vbox.children()) <= 65:
+            if len(self.created_buttons) < 64:
                 new_button = QPushButton(button_name, self)
-                self.vbox.addWidget(new_button)
+                new_button.clicked.connect(self.buttonClicked)
+
+                button_size = 50#(self.width() // self.max_columns) - 10
+                new_button.setFixedSize(button_size,button_size)
+
+                self.button_grid_layout.addWidget(new_button,self.current_row,self.current_column)
+                self.text_field.clear()
+                self.created_buttons[button_name] = new_button
+
+                self.current_column += 1
+                if self.current_column >= self.max_columns:
+                    self.current_row +=1
+                    self.current_column = 0
             else:
                 print("Maximum button limit reached.")
         else:
-            print("Button name cannot be empty.")
-        self.created_buttons = 
+            print("Button name cannot be empty or be named the same as any other buttons.")
+
+    def createStreams(self):
+        print(f"Creating LSL streams for:")
+        tag_backend.prepare_LSL_streaming(list(self.created_buttons.keys()))
+        
+
+    def buttonClicked(self):
+        clicked_button = self.sender()
+        print(f"Button {clicked_button.text()} has been clicked")
+        tag_backend.stream(clicked_button.text())
+
+
+    def keyPressEvent(self,event):
+        if event.key() == Qt.Key_Return or event.key() == Qt.Key_Enter:
+            print("hello?")
+            self.createButton()
+        else:
+            super().keyPressEvent(event)
+
 
 def main():
     app = QApplication(sys.argv)
